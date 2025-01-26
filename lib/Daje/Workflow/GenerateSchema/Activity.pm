fa451870012347cbb7faaac3534097649ba9ac12
janeskil1525 <janeskil1525@gmail.com>
2025-01-26 09:55:55 +0100

    First Commit

--- lib/Daje/Workflow/GenerateSchema/Activity.pm
+++ lib/Daje/Workflow/GenerateSchema/Activity.pm
@@ -1,39 +1,89 @@
 package Daje::Workflow::GenerateSchema::Activity;
-use 5.008001;
-use strict;
-use warnings;
+use Mojo::Base 'Daje::Workflow::Common::Activity::Base', -base, -signatures;
+
+use Mojo::JSON qw{to_json};
+use Daje::Workflow::GenerateSchema::Create::Schema;
+use Mojo::Pg;
+
+
+# NAME
+# ====
+#
+# Daje::Workflow::GenerateSchema::Activity - It pulls the schema out of a postgres database
+#
+# SYNOPSIS
+# ========
+#
+#     use Daje::Workflow::GenerateSchema::Activity;
+#
+# DESCRIPTION
+# ===========
+#
+# Daje::Workflow::GenerateSchema::Activity is ...
+#
+# LICENSE
+# =======
+#
+# Copyright (C) janeskil1525.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+# AUTHOR
+# ======
+#
+# janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
+#
+#
+#
 
 our $VERSION = "0.01";
 
 
 
+sub process ($self) {
+
+    my $schema = $self->_load_db_schema();
+    my $json = $self->_build_json($schema);
+    $self->_save_json($json);
+
+    return 1;
+}
+
+sub _create_database($self) {
+    Daje::Workflow::Database->new(
+        pg          => pg,
+        migrations  => $migrations,
+    )->migrate_from_file();
+}
+
+sub _load_db_schema($self) {
+
+    my $dbschema = Daje::Workflow::GenerateSchema::Create::Schema->new(
+        db => $self->db
+    )->get_db_schema('public');
+
+    return $dbschema;
+}
+
+sub _build_json($self, $schema) {
+    my $json = to_json($schema);
+
+    return $json;
+}
+
+sub _save_json($self, $json) {
+
+    my $path = $self->config->{DATABASE}->{output_dir};
+    open(my $fh, ">", $path . 'schema.json')
+        or die "could not open $path . 'schema.json";
+    print $fh $json;
+    close $fh;
+
+}
+
 1;
 __END__
 
-=encoding utf-8
 
-=head1 NAME
-
-Daje::Workflow::GenerateSchema::Activity - It's new $module
-
-=head1 SYNOPSIS
-
-    use Daje::Workflow::GenerateSchema::Activity;
-
-=head1 DESCRIPTION
-
-Daje::Workflow::GenerateSchema::Activity is ...
-
-=head1 LICENSE
-
-Copyright (C) janeskil1525.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
-
-=cut
 
--- lib/Daje/Workflow/GenerateSchema/Create/Schema.pm
+++ lib/Daje/Workflow/GenerateSchema/Create/Schema.pm
@@ -1,128 +1,125 @@
-use v5.40;
-use feature 'class';
-no warnings 'experimental::class';
+package Daje::Workflow::GenerateSchema::Create::Schema;
+use Mojo::Base  -base, -signatures;
 
-class Daje::Generate::Perl::CreateSchema {
 use Scalar::Util qw {reftype};
 use Syntax::Operator::Matches qw( matches mismatches );
 
-    field $db :param;
+has 'db';
 
-    method get_db_schema( $schema) {
-        $schema = 'public' unless $schema;
-        my @tab;
-        my @vie;
-        my @tables = ();
-        @tables = $self->_get_tables($schema);
-        my $length = scalar @tables;
-        for (my $i = 0; $i < $length; $i++) {
-            my $table->{table}->{table_name} = $tables[$i]->{table_name};
-            my $column_names = $self->_get_table_column_names($tables[$i]->{table_name}, $schema);
-            $table->{table}->{column_names} = $column_names;
-            my $indexes = $self->_get_table_indexes($tables[$i]->{table_name}, $schema);
-            if (defined $indexes) {
-                $table->{table}->{indexes} = $indexes;
+sub get_db_schema($self, $schema) {
+    $schema = 'public' unless $schema;
+    my @tab;
+    my @vie;
+    my @tables = ();
+    @tables = $self->_get_tables($schema);
+    my $length = scalar @tables;
+    for (my $i = 0; $i < $length; $i++) {
+        my $table->{table}->{table_name} = $tables[$i]->{table_name};
+        my $column_names = $self->_get_table_column_names($tables[$i]->{table_name}, $schema);
+        $table->{table}->{column_names} = $column_names;
+        my $indexes = $self->_get_table_indexes($tables[$i]->{table_name}, $schema);
+        if (defined $indexes) {
+            $table->{table}->{indexes} = $indexes;
+        }
+        push (@tab, $table);
+        my $temp = 1;
+    }
+    my $result->{tables} = \@tab;
+    my @views = $self->_get_views($schema);
+    $length = scalar @views;
+    for (my $i = 0; $i < $length; $i++ ) {
+        my $view->{view} = $views[$i];
+        my $column_names = $self->get_table_column_names($view->{view}->{table_name}, $schema);
+        $view->{view}->{column_names} = $column_names;
+        $view->{view}->{keys} = $self->_get_keys($column_names);
+        push (@vie, $view);
+    }
+
+    $result->{views} = \@vie;
+    return $result;
+}
+
+sub build_view_methods($self, $view, $column_names) {
+
+    my $methods->{table_name} = $view->{table_name};
+    $methods->{keys} = $self->_get_keys($column_names);
+    $methods->{create_endpoint} = 1;
+    my $method = $self->get_view_list($view->{table_name},$column_names);
+    push @{$methods->{methods}}, $method ;
+
+    return $methods;
+    }
+
+sub _get_keys($self, $column_names) {
+
+    my $keys->{has_companies} = 0;
+    $keys->{has_users} = 0;
+    $keys->{fk} = ();
+    my $length = scalar @{$column_names};
+    for (my $i = 0; $i < $length; $i++ ) {
+        if (length(@{$column_names}[$i]->{column_name}) > 0) {
+            if (index(@{$column_names}[$i]->{column_name},'_pkey') > -1){
+                $keys->{pk} = @{$column_names}[$i]->{column_name};
+            } elsif (@{$column_names}[$i]->{column_name} eq 'companies_fkey') {
+                $keys->{has_companies} = 1;
+            } elsif (@{$column_names}[$i]->{column_name} eq 'users_fkey') {
+                $keys->{has_users} = 1;
+            } elsif (index(@{$column_names}[$i]->{column_name},'_fkey') > -1) {
+                push @{$keys->{fk}}, @{$column_names}[$i]->{column_name};
             }
-            push (@tab, $table);
-            my $temp = 1;
         }
-        my $result->{tables} = \@tab;
-        my @views = $self->_get_views($schema);
-        $length = scalar @views;
-        for (my $i = 0; $i < $length; $i++ ) {
-            my $view->{view} = $views[$i];
-            my $column_names = $self->get_table_column_names($view->{view}->{table_name}, $schema);
-            $view->{view}->{column_names} = $column_names;
-            $view->{view}->{keys} = $self->_get_keys($column_names);
-            push (@vie, $view);
-        }
-
-        $result->{views} = \@vie;
-        return $result;
     }
+    return $keys;
+}
 
-    method build_view_methods($view, $column_names) {
+sub _get_tables($self, $schema) {
 
-        my $methods->{table_name} = $view->{table_name};
-        $methods->{keys} = $self->_get_keys($column_names);
-        $methods->{create_endpoint} = 1;
-        my $method = $self->get_view_list($view->{table_name},$column_names);
-        push @{$methods->{methods}}, $method ;
+    my @tables = $db->query(qq {
+        SELECT table_name
+          FROM information_schema.tables
+         WHERE table_schema = ?
+           AND table_type='BASE TABLE';
+    },($schema))->hashes;
+    @tables = @{ $tables[0] };
 
-        return $methods;
-    }
-
-    method _get_keys($column_names) {
-
-        my $keys->{has_companies} = 0;
-        $keys->{has_users} = 0;
-        $keys->{fk} = ();
-        my $length = scalar @{$column_names};
-        for (my $i = 0; $i < $length; $i++ ) {
-            if (length(@{$column_names}[$i]->{column_name}) > 0) {
-                if (index(@{$column_names}[$i]->{column_name},'_pkey') > -1){
-                    $keys->{pk} = @{$column_names}[$i]->{column_name};
-                } elsif (@{$column_names}[$i]->{column_name} eq 'companies_fkey') {
-                    $keys->{has_companies} = 1;
-                } elsif (@{$column_names}[$i]->{column_name} eq 'users_fkey') {
-                    $keys->{has_users} = 1;
-                } elsif (index(@{$column_names}[$i]->{column_name},'_fkey') > -1) {
-                    push @{$keys->{fk}}, @{$column_names}[$i]->{column_name};
-                }
-            }
-        }
-        return $keys;
-    }
-
-    method _get_tables($schema) {
-
-        my @tables = $db->query(qq {
-            SELECT table_name
-              FROM information_schema.tables
-             WHERE table_schema = ?
-               AND table_type='BASE TABLE';
-        },($schema))->hashes;
-        @tables = @{ $tables[0] };
-
-        return @tables;
-    }
+    return @tables;
+}
 
 
-    method _get_views($schema) {
+sub _get_views($self, $schema) {
 
-        my @views = $db->query(qq {
-            SELECT table_name
-                FROM
-                  information_schema.views
-                WHERE
-                  table_schema NOT IN (
-                    'information_schema', 'pg_catalog'
-                  ) AND table_schema = ?
-                ORDER BY table_name;
-        },($schema))->hashes;
-        @views =  @{$views[0]};
+    my @views = $db->query(qq {
+        SELECT table_name
+            FROM
+              information_schema.views
+            WHERE
+              table_schema NOT IN (
+                'information_schema', 'pg_catalog'
+              ) AND table_schema = ?
+            ORDER BY table_name;
+    },($schema))->hashes;
+    @views =  @{$views[0]};
 
-        return @views;
-    }
+    return @views;
+}
 
-    method _get_table_column_names($table, $schema) {
+sub _get_table_column_names($self, $table, $schema) {
 
-        $schema = 'public' unless $schema;
-        my @column_names = $db->query(
-            qq{
-                SELECT column_name
-                    FROM information_schema.columns
-                WHERE table_schema = ?
-                    AND table_name = ?
-            }, ($schema, $table)
-        )->hashes;
+    $schema = 'public' unless $schema;
+    my @column_names = $db->query(
+        qq{
+            SELECT column_name
+                FROM information_schema.columns
+            WHERE table_schema = ?
+                AND table_name = ?
+        }, ($schema, $table)
+    )->hashes;
 
-        @column_names = @{ $column_names[0] } if @{ $column_names[0] };
-        return \@column_names;
-    }
+    @column_names = @{ $column_names[0] } if @{ $column_names[0] };
+    return \@column_names;
+}
 
-method _get_table_indexes($table, $schema) {
-
+sub _get_table_indexes($self, $table, $schema) {
     $schema = 'public' unless $schema;
         my @column_names = $db->query(
             qq{
@@ -155,10 +152,10 @@ method _get_table_indexes($table, $schema) {
             }, ($schema, $table)
         )->hashes;
 
-        @column_names = @{ $column_names[0] } if @{ $column_names[0] };
-        return \@column_names;
-    }
+    @column_names = @{ $column_names[0] } if @{ $column_names[0] };
+    return \@column_names;
 }
+
 1;
 
 #################### pod generated by Pod::Autopod - keep this line to make pod updates possible ####################
